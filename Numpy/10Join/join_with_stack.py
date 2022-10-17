47ccedf9dd74ef7a051a0a75dff85a71600969ba
Nirob-0812 <mehedihasannirobcsediu@gmail.com>
2022-10-17 11:50:24 +0600

    3rd day

---
+++ Numpy/10Join/join_with_stack.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+arr=np.array([1,2,3,4])
+arr1=np.array([5,6,7,8])
+
+concatenate=np.stack((arr,arr1),axis=1)
+print(concatenate)
---
+++ Numpy/10Join/stack_in_column.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+arr=np.array([1,2,3,4])
+arr1=np.array([5,6,7,8])
+
+print(np.vstack((arr,arr1)))
