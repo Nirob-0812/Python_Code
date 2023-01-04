0dc0db4cb511d4c43953c87d450a52cbb92ba0f3
Nirob-0812 <mehedihasannirobcsediu@gmail.com>
2023-01-04 10:57:00 +0600

    7th

---
+++ NumPy Random/03Random Permutation/permutation_of_array.py
@@ -0,0 +1,5 @@
+from numpy import random
+import numpy as np
+
+arr=np.array([5,2,5,7,4])
+print(random.permutation(arr))
---
+++ NumPy Random/03Random Permutation/suffling_arrays.py
@@ -0,0 +1,6 @@
+from numpy import random
+import numpy as np
+
+arr=np.array([5,2,6,9,7])
+random.shuffle(arr)
+print(arr)
