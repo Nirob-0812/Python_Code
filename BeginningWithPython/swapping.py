4d1ead2e785a1ea39c935f9ccb30e745198be902
Nirob-0812 <mehedihasannirobcsediu@gmail.com>
2023-06-18 07:58:31 +0600

    swapping without third value

---
+++ BeginningWithPython/swapping.py
@@ -0,0 +1,18 @@
+a=10
+b=15
+a=a+b     #Using Subtraction
+b=a-b
+a=a-b
+print("A:",a,"\nB:",b)
+
+c=10
+d=5
+c=c^d    #Using XOR
+d=c^d
+c=c^d
+print("C:",c,"\nD:",d)
+
+e=20
+f=10
+e,f=f,e          #USing Assign
+print("E:",e,"\nF:",f)
